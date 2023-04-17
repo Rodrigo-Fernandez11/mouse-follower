2f3b389324cb9d7f216c61601f46b62cf5ee575e
Rodrigo-Fernandez11 <101433818+Rodrigo-Fernandez11@users.noreply.github.com>
2023-04-17 16:39:28 -0300

    proyecto react y typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    // Aquí puedes agregar reglas personalizadas de ESLint para tu proyecto
+    // Por ejemplo:
+    // 'no-console': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
